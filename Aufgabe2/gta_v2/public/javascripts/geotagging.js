b4077fe423324db321762bb67ae4650e363cf2ae
Oliver Reiser <reol1013@h-ka.de>
2022-11-14 11:44:14 +0100

    remove logs

--- Aufgabe2/gta_v2/public/javascripts/geotagging.js
+++ Aufgabe2/gta_v2/public/javascripts/geotagging.js
@@ -109,14 +109,12 @@ function updateLocation() {
     function setLocation(helper) {
         $("#tagLatitude").val(helper.latitude);
         $("#tagLongitude").val(helper.longitude);
-        console.log(helper.latitude);
-        console.log(helper.longitude);
 
         $("#discoveryLatitude").val(helper.latitude);
         $("#discoveryLongitude").val(helper.longitude);
         let mapManager = new MapManager("XtGxyGSmhZBkIbmGSOfBIoQ0Akq4OoUI");
         let mapUrl = mapManager.getMapUrl(parseFloat(helper.latitude), parseFloat(helper.longitude));
-        console.log(mapUrl);
+
         $("#mapView").attr("src", mapUrl);
     }
     
