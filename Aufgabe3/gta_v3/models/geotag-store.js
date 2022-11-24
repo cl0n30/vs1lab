3eaf72a6d83fae3925327667cacb93833314968e
Oliver Reiser <reol1013@h-ka.de>
2022-11-24 16:23:17 +0100

    implement geotag-store

--- Aufgabe3/gta_v3/models/geotag-store.js
+++ Aufgabe3/gta_v3/models/geotag-store.js
@@ -1,5 +1,8 @@
 // File origin: VS1LAB A3
 
+const GeoTag = require("./geotag");
+const GeoTagExamples = require("./geotag-examples");
+
 /**
  * This script is a template for exercise VS1lab/Aufgabe3
  * Complete all TODOs in the code documentation.
@@ -24,8 +27,84 @@
  * - Keyword matching should include partial matches from name or hashtag fields. 
  */
 class InMemoryGeoTagStore{
+    #geoTags = [];
+    #nearbyRadius = 500;
 
-    // TODO: ... your code here ...
+    constructor() {
+        let exampleTags = GeoTagExamples.tagList;
+        exampleTags.forEach((elem) => {
+            let tag = new GeoTag(elem[0], elem[1], elem[2], elem[3]);
+            this.#geoTags.push(tag);
+        });
+    }
+
+    /**
+     * 
+     * @param {GeoTag} geoTag 
+     */
+    addGeoTag(geoTag) {
+        this.#geoTags.push(geoTag);
+    }
+
+    /**
+     * 
+     * @param {string} geoTag 
+     */
+    removeGeoTag(tagName) {
+        for (let i = 0; i < this.#geoTags.length; i++) {
+            if (this.#geoTags[i].name == tagName) {
+                this.#geoTags.splice(i, 1);
+            }
+        }
+    }
+
+    /**
+     * 
+     * @param {GeolocationPosition} location 
+     * @returns {Array<GeoTag>}
+     */
+    getNearbyGeoTags(location) {
+        let lat = location.coords.latitude; 
+        let lon = location.coords.longitude;
+
+        //1 degree of latitude ~ 111111 m in y direction
+        //1 degree of longitude ~ 111111 * cos(lat) m in x direction
+        const oneDegreeInMeters = 111111;
+        const latOffset = this.#nearbyRadius / oneDegreeInMeters;
+        const lonOffset = this.#nearbyRadius / (oneDegreeInMeters * Math.cos(lat));
+
+        let maxLat = lat + latOffset;
+        let minLat = lat - latOffset;
+        let maxLon = lon + lonOffset;
+        let minLon = lon - lonOffset;
+
+        let nearbyTags = [];
+        this.#geoTags.forEach((tag) => {
+            let isInRadius = (tag.lat <= maxLat) && (tag.lon >= minLat) 
+                && (tag.long <= maxLon) && (tag.lon >= minLon);
+
+            if (isInRadius) {
+                nearbyTags.push(tag);
+            }
+        });
+        return nearbyTags;
+    }
+
+    /**
+     * 
+     * @param {GeolocationPosition} location 
+     * @param {string} query 
+     * @returns {Array<GeoTag>}
+     */
+    searchNearbyGeoTags(location, query) {
+        let nearbyTags = this.getNearbyGeoTags(location);
+        if (nearbyTags.length == 0) {
+            return [];
+        }
+
+        let foundTags = nearbyTags.filter(tag => tag.name.startsWith(query) || tag.hashtag.startsWith(query));
+        return foundTags;
+    }
 
 }
 
